c0354f5563c455b319fc6aac7543b6107415b786
hunter-ruggable <hunter.mathews@ruggable.com>
2025-10-24 12:09:33 -0500

    use existing bucket

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,19 +1,20 @@
 import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-  name: 'design-file-storage-amplify',
+  name: 'design-image-processing', // Replace with your existing bucket name
   isDefault: true,
-   access: (allow) => ({
+  bucketName: 'design-image-processing', // Specify the existing bucket name here
+  access: (allow) => ({
     'public/*': [
-        allow.guest.to(['read', 'write']),
-        allow.authenticated.to(['read', 'write', 'delete']),
+      allow.guest.to(['read', 'write']),
+      allow.authenticated.to(['read', 'write', 'delete']),
     ],
     'admin/*': [
-        allow.groups(['admin']).to(['read', 'write', 'delete']),
-        allow.authenticated.to(['read'])
+      allow.groups(['admin']).to(['read', 'write', 'delete']),
+      allow.authenticated.to(['read']),
     ],
     'private/{entity_id}/*': [
-        allow.entity('identity').to(['read', 'write', 'delete'])
-    ]
-   })
+      allow.entity('identity').to(['read', 'write', 'delete']),
+    ],
+  }),
 });
