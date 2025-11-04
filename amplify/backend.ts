2d8f05c5c2d619965c10db357df174b348ec943b
hunter-ruggable <hunter.mathews@ruggable.com>
2025-11-04 13:35:22 -0600

    add s3 backend for already existing resources

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,12 +1,76 @@
 import { defineBackend } from '@aws-amplify/backend';
+import { Effect, Policy, PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { Bucket } from "aws-cdk-lib/aws-s3";
 import { auth } from './auth/resource';
-import { storage } from './storage/resource';
+
 
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-defineBackend({
-  auth,
-  storage
+const backend = defineBackend({
+  auth
 });
+
+const customBucketStack = backend.createStack("design-image-processing-stack");
+
+// Import existing bucket
+const customBucket = Bucket.fromBucketAttributes(customBucketStack, "MyCustomBucket", {
+  bucketArn: "arn:aws:s3:::design-image-processing",
+  region: "us-west-2"
+});
+
+backend.addOutput({
+  storage: {
+    aws_region: customBucket.env.region,
+    bucket_name: customBucket.bucketName,
+    buckets: [
+      {
+        aws_region: customBucket.env.region,
+        bucket_name: customBucket.bucketName,
+        name: customBucket.bucketName,
+        paths: {
+          "unprocessed/*": {
+            guest: ["get", "list"],
+            authenticated: ["get", "list", "write", "delete"],
+          },
+        },
+      }
+    ]
+  },
+});
+
+// ... Unauthenticated/guest user policies and role attachments go here ...
+/*
+  Define an inline policy to attach to Amplify's auth role
+  This policy defines how authenticated users can access your existing bucket
+*/ 
+const authPolicy = new Policy(backend.stack, "customBucketAuthPolicy", {
+  statements: [
+    new PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: [
+        "s3:GetObject",
+        "s3:PutObject", 
+        "s3:DeleteObject"
+      ],
+      resources: [`${customBucket.bucketArn}/unprocessed/*`,],
+    }),
+    new PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: ["s3:ListBucket"],
+      resources: [
+        `${customBucket.bucketArn}`,
+        `${customBucket.bucketArn}/*`
+        ],
+      conditions: {
+        StringLike: {
+          "s3:prefix": ["unprocessed/*", "unprocessed/"],
+        },
+      },
+    }),
+  ],
+});
+
+// Add the policies to the authenticated user role
+backend.auth.resources.authenticatedUserIamRole.attachInlinePolicy(authPolicy);
